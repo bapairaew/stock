9f030826f95a69cd50f9a6ba75d70b9f8ad6edb2
Narudom <bapairaew@hotmail.com>
2017-02-21 17:04:30 +0700

    properly kill node tasks

--- windows/app/index.js
+++ windows/app/index.js
@@ -1,10 +1,13 @@
 const electron = require('electron');
+const ps = require('ps-node');
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
+const ipc = electron.ipcMain;
 
 const spawn = require('child_process').spawn;
 
 let mainWindow;
+const pids = [];
 
 function createWindow () {
   mainWindow = new BrowserWindow({
@@ -16,13 +19,27 @@ function createWindow () {
   // mainWindow.webContents.openDevTools();
   mainWindow.on('closed', function () {
     mainWindow = null;
-    const killnode = spawn('taskkill', ['/im', 'node.exe'], { cwd: process.cwd() });
   })
 }
+
+ipc.on('pid-message', function (e, arg) {
+  console.log('pid: ', arg);
+  pids.push(arg);
+});
 
 app.on('ready', createWindow);
 app.on('browser-window-created', function (e, window) {
   window.setMenu(null);
+});
+
+app.on('before-quit', function() {
+  pids.forEach(pid => {
+    console.log('killing: ', pid);
+    ps.kill(pid, err => {
+      if (err) throw new Error(err);
+      console.log('Process %s has been killed!', pid);
+    });
+  });
 });
 
 app.on('window-all-closed', function () {
